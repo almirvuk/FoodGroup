6af513b0f90b0d04206dd72f5e670edcbef45b31
Almir Vuk <almir.vuk@outlook.com>
2017-12-02 00:20:01 +0100

    search

--- FoodGroup/FoodGroup/Views/FoodTabPage.xaml.cs
+++ FoodGroup/FoodGroup/Views/FoodTabPage.xaml.cs
@@ -12,13 +12,29 @@ namespace FoodGroup.Views {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class FoodTabPage : ContentPage {
 
+        private List<Resturent> _foodList;
+
         public FoodTabPage(string categoryName, List<Resturent> foodList) {
             InitializeComponent();
 
             Title = categoryName;
             mealsListView.ItemsSource = foodList;
+
+            _foodList = foodList;
         }
 
+        private void Search(object sender, EventArgs e) {
 
+            stackLayoutShowHide.IsVisible = !stackLayoutShowHide.IsVisible;
+        }
+
+        private void searchChanged(object sender, TextChangedEventArgs e) {
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                mealsListView.ItemsSource = _foodList;
+            else
+                mealsListView.ItemsSource = _foodList.Where(i => i.Name.ToLower().Contains(e.NewTextValue.ToLower()));
+
+        }
     }
 }
